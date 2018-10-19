9393075db1010127d87532158b3b4075dea6e4eb
AbsoluteZero-CHN <caohao1209@gmail.com>
2018-10-19 22:54:58 +0800

    swarm测试

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.war" "$@"
+echo "再试一次?"
+echo "${CONSUL_SERVER_HOST}"
+echo "${JHIPSTER_SLEEP}"
+exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.war --spring.cloud.consul.host=${CONSUL_SERVER_HOST}" "$@"
